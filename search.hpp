72c39a8f72ced9a0808f4872a6e6ec58b0a81980
silvercobraa <silver.cobra1n@gmail.com>
2018-04-26 23:09:46 -0300

    Se corrigió A* a su versión con punteros

--- search.hpp
+++ search.hpp
@@ -40,26 +40,7 @@ bool dfs(Node* root, int depth, int max_depth) {
 	return dfs(visited, root, depth, max_depth);
 }
 
-/*
-// TODO: imprimir correctamente la traza de movimientos: "move[s]"
-void print_trace(map<string, string>& parent, map<string, string>& move, string s) {
-	vector<string> path;
-	while (s != "") {
-		// cout << s << endl;
-		Node n = {s, NULL};
-		// cout << move[s] << " ";
-		path.push_back(move[s]);
-		s = parent[s];
-		// print(n);
-	}
-	while (!path.empty()) {
-		cout << path.back() << ' ';
-		path.pop_back();
-	}
-	cout << endl;
-}
 
-*/
 bool bfs(Node* root) {
 	queue<Node*> q;
 	set<string> v;
@@ -89,53 +70,43 @@ bool bfs(Node* root) {
 	cout << "DIDN'T REACH GOAL STATE" << endl;
 	return false;
 }
-/*
-bool A_star(Node& node) {
-	set<pair<double,Node>> q;
-	set<string> v; // los estados visitados
-	map<string, string> move; // el movimiento con el cual se llego a este estado
-	map<string, string> parent_state; // el estado anterior
 
-	q.insert({0, node});
-	parent_state[node.state] = "";
-	move[node.state] = "";
+
+bool A_star(Node* root) {
+	set<pair<double,Node*>> q;
+	set<string> v; // los estados visitados
+
+	q.insert({0, root});
 
 	while(!q.empty()) {
 		cout << v.size() << endl;
 		// cout << q.size() << endl;
-		Node parent = (*q.begin()).second;
-		// print(parent);
+		Node* parent = (*q.begin()).second;
 		q.erase(q.begin());
-		v.insert(parent.state);
+		v.insert(parent->state);
 		if (goal_test(parent)) {
 			cout << "REACHED GOAL STATE" << endl;
-			print_trace(parent_state, move, parent.state);
+			solution(parent);
 			return true;
 		}
 		// if (v.find(parent.state) != v.end()) {
 		// 	continue;
 		// }
 		for (int act = 0; act < actions.size(); act++) {
-			// auto child = actions[i](parent);
-			auto child = child_node(parent, act);
-			if (v.find(child.state) == v.end()) {
-				move[child.state] = action_name[act];
-				// cout << child.state << " -> " << act.first << endl;
-				parent_state[child.state] = parent.state;
-				// print(parent);
-				// cout << act.first << endl;
-				// print(child);
+			Node* child = child_node(parent, act);
+			if (v.find(child->state) == v.end()) {
 				// DECOMENTAR ACA PARA BÚSQUEDA GREEDY
 				// q.insert({heuristic(child), child});
 				// DESCOMENTAR ACA PARA BÚSCQUEDA A*
-				q.insert({child.cost + heuristic(child), child});
+				q.insert({child->cost + heuristic(child), child});
 			}
 		}
 	}
 	cout << "DIDN'T REACH GOAL STATE" << endl;
 	return false;
 }
-*/
+
+
 // iterative deepening search
 void ids(Node* root) {
 	int max_depth = 1;
