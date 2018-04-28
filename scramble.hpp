3364383b09928aa7a57143f40a4dfc4dfadf183a
silvercobraa <silver.cobra1n@gmail.com>
2018-04-28 00:03:07 -0300

    Commit irrelevante

--- scramble.hpp
+++ scramble.hpp
@@ -14,15 +14,12 @@ void scramble(Node* root, std::string moves) {
 	auto ss = std::stringstream(moves);
 	std::string move;
 	while (ss >> move) {
-		std::cout << move << endl;
-		// root = action_by_name[move](root);
 		root->state = action_by_name[move](root->state);
-		std::cout << root->state << endl;
 	}
 	return;
 }
 
-Node scramble(Node root, int moves) {
+Node random_scramble(Node root, int moves) {
 	// TODO: implementar esta wea
 	Node scrambled;
 	return scrambled;
